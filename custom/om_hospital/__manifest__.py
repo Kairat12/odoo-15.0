3f7e917658e5da13c9fba045aba5f1dc8a66bd07
Kairat12 <56346130+Kairat12@users.noreply.github.com>
2022-10-12 15:03:15 +0600

    Initial commit

---
+++ custom/om_hospital/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Hospital Managment',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'Kairat',
+    'sequence': -100,
+    'summary': 'Hospital managment system',
+    'description': """Hospital managment system""",
+    'depends': [],
+    'data': [
+        'views/menu.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': ''
+
+}
